01355801ba1c2209620b857e6d441898b17755df
FrankMugagga <frank.mugagga@gmail.com>
2023-12-27 22:23:04 +0300

    Clear linter errors

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-let speakerSection = document.getElementById('speakerSection');
+const speakerSection = document.getElementById('speakerSection');
 
 const speakers = [
-    {
-      image: 'images/speakers/Brendan_Eich.jpg',
-      name: 'Brendan Eich',
-      profession: 'American computer programmer',
-      about: 'Brendan Eich is an American computer programmer and technology executive.',
-    },
-    {
-        image: 'images/speakers/Dennis_Ritchie.jpg',
-        name: 'Dennis Ritchie',
-        profession: 'American computer scientist',
-        about: 'Dennis MacAlistair Ritchie was an American computer scientist.',
-    },
-    {
-        image: 'images/speakers/ken_thompson.jpg',
-        name: 'Ken Thompson',
-        profession: 'American computer designer',
-        about: 'Kenneth Lane Thompson is an American pioneer of computer science.'
-    },
-    {
-        image: 'images/speakers/Larry_Page.jpg',
-        name: 'Larry Page',
-        profession: 'American business magnate',
-        about: 'Lawrence Edward Page is an American business magnate, computer scientist and internet entrepreneur.',
-    },
-    {
-        image: 'images/speakers/Margaret_Hamilton.jpg',
-        name: 'Margaret Hamilton',
-        profession: 'American computer scientist',
-        about: 'Margaret Heafield Hamilton is an American computer scientist.',
-    },
-    {
-        image: 'images/speakers/Radia-Perlman.jpg',
-        name: 'Radia Perlman',
-        profession: 'American network engineer',
-        about: 'Radia Joy Perlman is an American computer programmer and network engineer.',
-    },
+  {
+    image: 'images/speakers/Brendan_Eich.jpg',
+    name: 'Brendan Eich',
+    profession: 'American computer programmer',
+    about: 'Brendan Eich is an American computer programmer and technology executive.',
+  },
+  {
+    image: 'images/speakers/Dennis_Ritchie.jpg',
+    name: 'Dennis Ritchie',
+    profession: 'American computer scientist',
+    about: 'Dennis MacAlistair Ritchie was an American computer scientist.',
+  },
+  {
+    image: 'images/speakers/ken_thompson.jpg',
+    name: 'Ken Thompson',
+    profession: 'American computer designer',
+    about: 'Kenneth Lane Thompson is an American pioneer of computer science.',
+  },
+  {
+    image: 'images/speakers/Larry_Page.jpg',
+    name: 'Larry Page',
+    profession: 'American business magnate',
+    about: 'Lawrence Edward Page is an American business magnate, computer scientist and internet entrepreneur.',
+  },
+  {
+    image: 'images/speakers/Margaret_Hamilton.jpg',
+    name: 'Margaret Hamilton',
+    profession: 'American computer scientist',
+    about: 'Margaret Heafield Hamilton is an American computer scientist.',
+  },
+  {
+    image: 'images/speakers/Radia-Perlman.jpg',
+    name: 'Radia Perlman',
+    profession: 'American network engineer',
+    about: 'Radia Joy Perlman is an American computer programmer and network engineer.',
+  },
 ];
 
-let speaker_content = '';
+let speakerContent = '';
 
 speakers.forEach((speaker) => {
-    speaker_content += `
+  speakerContent += `
       <li>
         <div id="img_background">
           <img src=${speaker.image} alt=${speaker.name}>
@@ -58,4 +58,4 @@ speakers.forEach((speaker) => {
     `;
 });
 
-speakerSection.innerHTML = speaker_content;
+speakerSection.innerHTML = speakerContent;
